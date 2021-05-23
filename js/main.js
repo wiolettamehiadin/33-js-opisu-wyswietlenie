76b179cb2e6b6c202c382a971c05e571e12c27f2
wiolettamehiadin <wioletta_k@live.co.uk>
2021-05-23 21:51:52 +0100

    Rozpoczecie pliku

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let myParagraf = document.getElementById("opis");
+
+let myButton = document.querySelector('button');
+document.querySelector('button').onclick = function () {
+    alert('Hey, milego dnia!')
+}
