f8d57312a2e012ee13d7068cb6533716bf4d5f01
wiolettamehiadin <wioletta_k@live.co.uk>
2021-05-25 21:26:52 +0100

    Poprawka

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ let myParagraf = document.getElementById("opis");
 
 let myButton = document.querySelector('button');
 document.querySelector('button').onclick = function () {
-    alert('Hey, milego dnia!')
+     myParagraf.textContent = ('Hey, milego dnia!');
 }
